66b2e5375d31035fd2a132b0d031b4c17513bda1
JUAN CAMILO CAPERA B <capera.juan@correounivalle.edu.co>
2023-06-22 16:54:10 -0500

    Configurando ventana principal

---
+++ src/Vista/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vista;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author juan-
+ */
+public class Principal {
+    public static void main(String[] args) {
+        NewJFrame ventana = new NewJFrame();
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+    }
+}
